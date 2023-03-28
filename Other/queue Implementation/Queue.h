7484af58916efa36d6bc6da14c14a601f3a07c64
Omar Abbas <115028809+Omar7001-B@users.noreply.github.com>
2023-03-28 12:57:36 +0200

    Update Queue.h

--- Other/queue Implementation/Queue.h
+++ Other/queue Implementation/Queue.h
@@ -1,18 +1,16 @@
-//
-// Created by Omar on 3/28/2023.
-//
 template<typename T>
 class Queue {
 private:
-	T* arr;
-	int i_front;
-	int i_back;
+    T* arr;
+    int capacity;
+    int i_front;
+    int i_back;
 public:
-	Queue(int size);
-	~Queue();
-	void push(T val);
-	T front();
-	void pop();
-	int size();
-	bool empty();
+    Queue(int size);
+    ~Queue();
+    void push(T val);
+    T front();
+    void pop();
+    int size();
+    bool empty();
 };
